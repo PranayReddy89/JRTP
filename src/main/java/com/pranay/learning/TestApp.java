5bcbf6f3940bd4f2da4995dcdf5368fc1f783d52
Pranay Reddy <pranaykallem.reddy@gmail.com>
2021-11-14 22:13:39 +0530

    feature-02 changes

--- src/main/java/com/pranay/learning/TestApp.java
+++ src/main/java/com/pranay/learning/TestApp.java
@@ -1,10 +1,14 @@
 package com.pranay.learning;
 
 public class TestApp {
-
+    private int sum;
 	public static void main(String[] args) {
 		System.out.println("Develop branch");
 
+	}
+	public void add()
+	{
+		//logic
 	}
 
 }
